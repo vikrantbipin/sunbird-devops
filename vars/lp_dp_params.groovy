00c7646b5fd0e4b537fb96ac087b194bf98fb1f6
beepdot <3581357+beepdot@users.noreply.github.com>
2024-05-30 17:42:11 +0530

    fix: add debug logs

--- vars/lp_dp_params.groovy
+++ vars/lp_dp_params.groovy
@@ -24,17 +24,24 @@ def call(){
                    Setting build_number to lastSuccessfulBuild to copy metadata.json
                     '''.stripIndent().replace("\n", " ") + ANSI_NORMAL)
                     buildNumber = "lastSuccessfulBuild"
-                } else
+                    println("Build number: " + buildNumber)
+                } 
+                else
+                {
                     buildNumber = params.build_number
+                    println("Build number: " + buildNumber)
+                }
 
                 values = [:]
                 try {
                     if (buildNumber == "lastSuccessfulBuild")
                     {
+                        println("Using lastSuccessfulBuild" + buildNumber)
                         copyArtifacts projectName: params.absolute_job_path, fingerprintArtifacts: true, flatten: true, selector: lastSuccessful()
                     }
                     else
                     {
+                        println("Build number: " + buildNumber)
                         copyArtifacts projectName: params.absolute_job_path, fingerprintArtifacts: true, flatten: true, selector: specific(buildNumber)
                     }
                 }
